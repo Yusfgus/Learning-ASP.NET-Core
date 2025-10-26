f2cf5cbbc052bc39dc7caf4f439caece4a83703b
UC <yousefgus111@gmail.com>
2025-10-26 09:50:46 +0300

    ADO.NET SelectRawSql

---
+++ ADO.NET/Program.cs
@@ -0,0 +1,17 @@
+﻿using Shared;
+
+namespace ADO.NET;
+
+class Program
+{
+    static void Main()
+    {
+        Utils.printTitle(title: "Hello World! ( ADO.NET )", color: ConsoleColor.Red, width: 80);
+
+        RawSql.ConnectionString();
+
+        RawSql.Select();
+
+        // Console.ReadKey();
+    }
+}
