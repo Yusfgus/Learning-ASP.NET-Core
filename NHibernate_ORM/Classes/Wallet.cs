cdfe8e05f88f9756b23737a319ab0a35617f5202
UC <yousefgus111@gmail.com>
2025-10-27 18:44:25 +0300

    NHibernate Class Mapping

---
+++ NHibernate_ORM/Classes/Wallet.cs
@@ -0,0 +1,13 @@
+namespace NHibernate_ORM.Classes;
+
+public class Wallet
+{
+    public virtual int Id { get; set; }
+    public virtual string? Holder { get; set; }
+    public virtual decimal? Balance { get; set; }
+
+    public override string ToString()
+    {
+        return $"[{Id}] {Holder} ({Balance:C})";
+    }
+}
---
+++ NHibernate_ORM/Program.cs
@@ -0,0 +1,42 @@
+﻿using Shared;
+
+namespace NHibernate_ORM;
+
+class Program
+{
+    static void Main()
+    {
+        Utils.printTitle(title: "Hello World! ( NHibernate )", color: ConsoleColor.Red, width: 80);
+
+        Session.CreateSession();
+
+        string? choice;
+        bool flag = true;
+        while (flag)
+        {
+            Console.Write("""
+            Enter a choice:
+            1) .
+            Any other key to exit.
+            ---> 
+            """);
+
+            choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    
+                    break;
+                default:
+                    flag = false;
+                    break;
+            }
+
+            Console.WriteLine("──────────────────────────────────────────────────────────────────\n");
+        }
+
+
+        // Console.ReadKey();
+    }
+}
