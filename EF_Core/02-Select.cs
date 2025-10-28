13863013e10c8e2d9901f9a9c5710cbeb531bbba
UC <yousefgus111@gmail.com>
2025-10-29 02:45:11 +0300

    EF-Core DbContext ( Dependency Injection )

--- EF_Core/02-Select.cs
+++ EF_Core/02-Select.cs
@@ -2,6 +2,7 @@ using Shared;
 using EF_Core.Entities;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EF_Core;
 
@@ -9,9 +10,8 @@ public abstract class Select
 {
     public static void AllData()
     {
-        Utils.printTitle(title: "Select ( All Data )", color: ConsoleColor.Blue, width: 70);
-        
-        #region use external configuration DbContext
+        Utils.printTitle(title: "Select All Data ( using External DbContext configuration )", color: ConsoleColor.Blue, width: 70);
+
         var configuration = new ConfigurationBuilder()
            .AddJsonFile("appsettings.json")
            .Build();
@@ -23,7 +23,6 @@ public abstract class Select
         optionsBuilder.UseSqlServer(constr);
 
         var options = optionsBuilder.Options;
-        #endregion
 
         using (var context = new AppDbContext02(options))
         {
@@ -33,15 +32,29 @@ public abstract class Select
 
     public static void ById()
     {
-        Utils.printTitle(title: "Select ( By ID )", color: ConsoleColor.Blue, width: 70);
+        Utils.printTitle(title: "Select By ID ( using Dependency Injection + External DbContext configuration )", color: ConsoleColor.Blue, width: 70);
 
         Console.Write("Enter wallet id: ");
         int id = Convert.ToInt32(Console.ReadLine());
 
-        using (var context = new AppDbContext01())
+        var config = new ConfigurationBuilder()
+          .AddJsonFile("appsettings.json")
+          .Build();
+
+        var connectionString = config.GetSection("constr").Value;
+
+        var services = new ServiceCollection();
+
+        services.AddDbContext<AppDbContext02>(options =>
+            options.UseSqlServer(connectionString)
+        );
+
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+        using (var context = serviceProvider.GetService<AppDbContext02>())
         {
             // Wallet? wallet = context.Wallets.FirstOrDefault(x => x.Id == id);
-            Wallet? wallet = context.Wallets.SingleOrDefault(x => x.Id == id);
+            Wallet? wallet = context!.Wallets.SingleOrDefault(x => x.Id == id);
 
             Console.WriteLine(wallet);
         }
@@ -51,9 +64,21 @@ public abstract class Select
     {
         Utils.printTitle(title: "Select ( Where )", color: ConsoleColor.Blue, width: 70);
 
-        using (var context = new AppDbContext01())
+        var config = new ConfigurationBuilder()
+          .AddJsonFile("appsettings.json")
+          .Build();
+
+        var connectionString = config.GetSection("constr").Value;
+
+        var services = new ServiceCollection();
+
+        services.AddDbContext<AppDbContext01>();
+
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+        using (var context = serviceProvider.GetService<AppDbContext01>())
         {
-            IQueryable<Wallet> result = context.Wallets.Where(x => x.Balance > 2000);
+            IQueryable<Wallet> result = context!.Wallets.Where(x => x.Balance > 2000);
 
             result.Print("Wallets Where Balance > 2000");
         }
