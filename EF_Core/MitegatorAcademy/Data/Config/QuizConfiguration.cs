438e98223e999641d4e04f2a3e6342a92e303727
UC <yousefgus111@gmail.com>
2025-11-03 22:02:56 +0200

    add Quiz class

---
+++ EF_Core/MitegatorAcademy/Data/Config/QuizConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using EF_Core.MitegatorAcademy.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EF_Core.MitegatorAcademy.Data.Config;
+
+public class QuizConfiguration : IEntityTypeConfiguration<Quiz>
+{
+    public void Configure(EntityTypeBuilder<Quiz> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedNever();
+
+        builder.UseTpcMappingStrategy();
+    }
+}
