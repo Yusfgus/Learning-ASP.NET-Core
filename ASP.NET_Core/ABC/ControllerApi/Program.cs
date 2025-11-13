ace1ed5ea9cd9db0bf204d7ec2137c9082b48f06
UC <yousefgus111@gmail.com>
2025-11-13 22:04:57 +0200

    Middleware

--- ASP.NET_Core/ABC/ControllerApi/Program.cs
+++ ASP.NET_Core/ABC/ControllerApi/Program.cs
@@ -1,5 +1,9 @@
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using ABC.Controller.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +17,22 @@ builder.Services.AddScoped<IBookService, BookService>();
 var app = builder.Build();
 
 // request pipeline
+
+app.Use(async (context, next) =>
+{
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+
+    context.Response.OnStarting(() =>
+    {
+        context.Response.Headers.Append("X-response-delay-ms", stopwatch.ElapsedMilliseconds.ToString());
+
+        return Task.CompletedTask;
+    });
+
+    await next(context);
+});
+
 app.MapControllers();
 
 app.Run();
