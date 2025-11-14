8921790ff6b7b8582ed854e1c5009a9ab439cd70
UC <yousefgus111@gmail.com>
2025-11-14 20:54:57 +0200

    Design Pattern - Singleton ( Thread Safe Using Lazy Loading )

---
+++ Design_Pattern/Singletone/06-ThreadSafeUsingLazyLoading/LogMessage.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Design_Pattern.Singleton.ThreadSafeUsingLazyLoading;
+
+public class LogMessage
+{
+    public string Message { get; set; }
+    public LogType LogType { get; set; } 
+    public DateTime CreatedAt { get; set; }
+
+    public override string ToString()
+    {
+        var timestamp = CreatedAt.ToString("yyyy-MM-dd hh:mm");
+        
+        return $"{LogType.ToString().PadLeft(7, ' ')}  [{timestamp}] {Message}";
+    }
+}
---
+++ Design_Pattern/Singletone/06-ThreadSafeUsingLazyLoading/LogType.cs
@@ -0,0 +1,8 @@
+    namespace Design_Pattern.Singleton.ThreadSafeUsingLazyLoading;
+    
+    public enum LogType
+    {
+        INFO,
+        WARNING,
+        ERROR
+    }
