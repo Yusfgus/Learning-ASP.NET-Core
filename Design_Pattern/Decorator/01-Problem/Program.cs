2796d66dcfc383412bb02075b4666f2e269e6fcc
UC <yousefgus111@gmail.com>
2025-11-18 19:45:15 +0200

    Design Patter - Decorator ( Problem )

---
+++ Design_Pattern/Decorator/01-Problem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Shared;
+
+namespace Design_Pattern.Decorator.Problem;
+
+public class Program
+{
+    public static void Main()
+    {
+        Utils.printTitle("Design Patter", 80, ConsoleColor.Red);
+        Utils.printTitle("Decorator ( Problem )", 70, ConsoleColor.Blue);
+
+        var order = new IceCreamWithSprinkles();
+        Console.WriteLine(order);
+
+        // the problem appears with combinations 
+        // Ice Cream + sprinkles + fruit
+        // Ice Cream + sprinkles + chocolate chips
+    }
+}
