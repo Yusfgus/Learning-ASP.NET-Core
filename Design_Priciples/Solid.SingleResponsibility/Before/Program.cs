b188887acb2c08eb6ba64ffaa2b6ffc884a7c353
UC <yousefgus111@gmail.com>
2025-11-19 19:50:15 +0200

    Design Principles - Solid - Single Responsibility ( Before )

---
+++ Design_Priciples/Solid.SingleResponsibility/Before/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Shared;
+
+namespace Design_Principles.Solid.SingleResponsibility.Before;
+
+public class Program
+{
+    public static void Main()
+    {
+        Utils.printTitle("Design Principles", 80, ConsoleColor.Red);
+        Utils.printTitle("Solid: Single Responsibility ( Before )", 60, ConsoleColor.Blue);
+
+        var account = new Account("Reem", "reem@example.com", 10000m);
+        account.MakeTransaction(500);
+        account.MakeTransaction(-11000);
+    }
+}
