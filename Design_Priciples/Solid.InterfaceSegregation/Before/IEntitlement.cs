6bc92dd652457c415a333828123f664a2397801b
UC <yousefgus111@gmail.com>
2025-11-21 18:35:43 +0200

    Design Principles - Solid - Interface Segregation ( Before )

---
+++ Design_Priciples/Solid.InterfaceSegregation/Before/IEntitlement.cs
@@ -0,0 +1,12 @@
+﻿namespace Design_Principles.Solid.InterfaceSegregation.Before;
+
+interface IEntitlement
+{
+    decimal CalculatePension();
+    decimal CalculateHealthInsurance();
+    decimal CalculateRentalSubsidy();
+    decimal CalculateBonuses();
+    decimal CalculateTransportationReimbursement();
+
+}
+
---
+++ Design_Priciples/Solid.InterfaceSegregation/Before/Program.cs
@@ -0,0 +1,21 @@
+﻿using Shared;
+using System;
+
+namespace Design_Principles.Solid.InterfaceSegregation.Before;
+
+class Program
+{
+    static void Main()
+    {
+        Utils.printTitle("Design Principles", 80, System.ConsoleColor.Red);
+        Utils.printTitle("Interface Segregation ( Before )", 60, System.ConsoleColor.Blue);
+
+        var employees = Repository.LoadEmployees();
+
+        foreach (var e in employees)
+        {
+            Console.WriteLine(e.PrintSalarySlip());
+            Console.WriteLine();
+        }
+    }
+}
