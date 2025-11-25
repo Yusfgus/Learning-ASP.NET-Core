b588be730647a593d6ae1f5de0cbac7beb8d9660
UC <yousefgus111@gmail.com>
2025-11-26 01:14:02 +0200

    Module#06 - Dependency Injection ( Multiple Registrations )

---
+++ ASP.NET_Core/Course/Module#06 - Dependency Injection (DI, IOC)/06-MultipleRegistrations/Program.cs
@@ -0,0 +1,67 @@
+
+using System.Text;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddTransient<IDependency, DependencyV1>();
+builder.Services.AddTransient<IDependency, DependencyV1>(); // will add
+
+builder.Services.AddTransient<IDependency, DependencyV2>();
+builder.Services.TryAddTransient<IDependency, DependencyV2>(); // won't add due to repetition 
+
+
+var app = builder.Build();
+
+
+app.MapGet("/single", (IDependency dependency) =>
+{
+    string result = dependency.DoSomething();
+
+    return Results.Ok(result);
+});
+
+app.MapGet("/multiple", (IEnumerable<IDependency> dependencies) =>
+{
+    string result = dependencies
+                    .Select(d => d.DoSomething())
+                    .Aggregate(
+                        new StringBuilder(),
+                        (sb, x) => sb.Append(x).Append(", "),
+                        sb => sb.ToString().TrimEnd(',', ' ')
+                    );
+
+    return Results.Ok(result);
+});
+
+app.Map("/idependency-registrations", (IServiceProvider serviceProvider) =>
+{
+    var servicesRegisteredCount = serviceProvider.GetServices<IDependency>().Count();
+    
+    return Results.Ok(servicesRegisteredCount);
+});
+
+
+app.Run();
+
+interface IDependency
+{
+    string DoSomething();
+}
+
+class DependencyV1 : IDependency
+{
+    public string DoSomething()
+    {
+        return "Doing something V1";
+    }
+}
+
+class DependencyV2 : IDependency
+{
+    public string DoSomething()
+    {
+        return "Doing something V2";
+    }
+}
