1da367304905f3ea1091b1b88f98c6c9ad46536d
UC <yousefgus111@gmail.com>
2025-12-10 00:15:51 +0200

    Module#17 - Error Handling ( Global Error Handling )

---
+++ ASP.NET_Core/Course/Module#17 - Error Handling/06-GlobalErrorHandling/Endpoints/ErrorEndpoints.cs
@@ -0,0 +1,98 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalErrorHandling.Endpoints;
+
+public static class ErrorEndpoints
+{
+    public static RouteGroupBuilder MapErrorEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/api/minimal-fake-errors");
+
+        group.MapGet("/server-error", ServerErrorExample);
+
+        group.MapPost("/bad-request", BadRequestExample);
+        group.MapPost("/bad-request-1", BadRequestExample1);
+        group.MapPost("/bad-request-2", BadRequestExample2);
+
+        group.MapPost("/not-found", NotFoundExample);
+        group.MapPost("/not-found-1", NotFoundExample1);
+        group.MapPost("/not-found-2", NotFoundExample2);
+
+        group.MapPost("/conflict", ConflictExample);
+        group.MapPost("/conflict-1", ConflictExample1);
+        group.MapPost("/conflict-2", ConflictExample2);
+
+        group.MapPost("/unauthorized", () => Results.Unauthorized());
+
+        group.MapPost("/business-rule-error", () =>
+        {
+            throw new ValidationException("A discontinued product cannot be put on promotion.");
+        });
+
+        return group;
+    }
+
+    public static IResult ServerErrorExample()
+    {
+        File.ReadAllText(@"C:\Settings\UploadSettings.json"); // not exist
+        return Results.Created();
+    }
+    
+    //==============================================================================
+
+    private static IResult BadRequestExample()
+        => Results.BadRequest();
+
+    private static IResult BadRequestExample1()
+        => Results.BadRequest("Product SKU is required");
+
+    private static IResult BadRequestExample2() => Results.Problem(
+        type: "http://example.com/prop/sku-required",
+        title: "Bad Request",
+        statusCode: StatusCodes.Status400BadRequest,
+        detail: "Product SKU is required"
+    );
+
+    //==============================================================================
+
+    private static IResult NotFoundExample()
+        => Results.NotFound();
+
+    private static IResult NotFoundExample1()
+        => Results.NotFound("Product not found.");
+
+    private static IResult NotFoundExample2() => Results.Problem(
+        type: "http://example.com/prop/product-not-found",
+        title: "Not Found",
+        statusCode: StatusCodes.Status404NotFound,
+        detail: "Product not found."
+    );
+
+    //==============================================================================
+
+    private static IResult ConflictExample() 
+        => Results.Conflict();
+
+    private static IResult ConflictExample1() 
+        => Results.Conflict("This Product already exists.");
+
+    private static IResult ConflictExample2() => Results.Problem(
+        type: "http://example.com/prop/product-not-found",
+        title: "Conflict",
+        statusCode: StatusCodes.Status409Conflict,
+        detail: "This Product already exists."
+    );
+
+    //==============================================================================
+
+    private static IResult UnauthorizedExample1()
+        => Results.Unauthorized();
+
+    //==============================================================================
+
+    private static IResult BusinessRuleExample() 
+        => throw new ValidationException("A discontinued product cannot be put on promotion.");
+
+}
+
---
+++ ASP.NET_Core/Course/Module#17 - Error Handling/06-GlobalErrorHandling/Exceptions/GlobalExceptionHander.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalErrorHandling.Exceptions;
+
+public class GlobalExceptionHandler(IProblemDetailsService problemDetailsService) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        httpContext.Response.StatusCode = exception switch
+        {
+            ValidationException => StatusCodes.Status400BadRequest,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return await problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+        {
+            HttpContext = httpContext,
+            Exception = exception,
+            ProblemDetails = new ProblemDetails
+            {
+                Type = exception.GetType().Name,
+                Title = "Error has occurred",
+                Detail = exception.Message
+            }
+        });
+    }
+}
